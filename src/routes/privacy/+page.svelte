28a6f9aec97a65f1fb024439d01b01614aa765bf
trivernis <trivernis@protonmail.com>
2024-07-06 14:32:41 +0200

    Add privacy site

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Box from "../../components/atoms/Box.svelte";
+</script>
+
+<Box>
+  <h2>Privacy</h2>
+  <p>
+    I don't store any personal information about you. This website does not use
+    advertisement or any metrics collector. No information is sucked from your
+    computer. Nothing is tracked. No fingerprints are being collected. If you
+    need more information, contact me at
+    <a href="mailto:admin@trivernis.net">admin@trivernis.net</a>.
+  </p>
+</Box>
