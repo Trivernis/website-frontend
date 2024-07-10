1593c2f97cb11ac1f347b7d82aed47127819b5ca
trivernis <trivernis@protonmail.com>
2024-07-10 23:36:28 +0200

    Move buttons to separate file

---
+++ src/lib/buttons.ts
@@ -0,0 +1,49 @@
+export default [
+	{
+		src: "/buttons/adobe_getflash4.gif",
+		alt: "Get Adobe Flash Player.",
+	},
+	{ src: "/buttons/get_firefox.gif", alt: "Get Firefox." },
+	{
+		src: "/buttons/nft_no_fucking_thanks.gif",
+		alt: "NFT? No Fucking Thanks.",
+	},
+	{ src: "/buttons/javanow.gif", alt: "Java. Get it now." },
+	{ src: "/buttons/wget.gif", alt: "I surf with wget. And you?" },
+	{ src: "/buttons/bestview.gif", alt: "Best viewed with open eyes." },
+	{ src: "/buttons/html_learn_it_today.gif", alt: "HTML. Learn it today." },
+	{
+		src: "/buttons/buy_kofi.gif",
+		alt: "Buy Ko-Fi.",
+		href: "https://ko-fi.com/trivernisdev",
+	},
+	{
+		src: "https://dimden.dev/services/images/88x31.gif",
+		alt: "dimden.dev",
+		href: "https://dimden.dev/",
+	},
+	{
+		src: "https://www.tanguy.cyou/assets/img/links/button.webp",
+		alt: "tanguy.cyou",
+		href: "https://www.tanguy.cyou/",
+	},
+	{
+		src: "https://coolbugs.win/media/coolbugs-88x31.gif",
+		alt: "coolbugs.win",
+		href: "https://coolbugs.win",
+	},
+	{
+		src: "https://screamingegg.neocities.org/0_assets/pics/index/buttons/screamingegg_button-1.jpg",
+		alt: "screamingegg",
+		href: "https://screamingegg.neocities.org/",
+	},
+	{
+		src: "https://file.garden/ZZSxuqU5uT7zKhQh/webrsc/buttonz50.gif",
+		alt: "sevs funhouse",
+		href: "https://zoneoffun.neocities.org/",
+	},
+] as {
+	src: string;
+	alt: string;
+	href?: string;
+}[];
