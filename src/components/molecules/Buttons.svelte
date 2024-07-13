3cc0194f679f208d3cadf4c31bc348889e825409
trivernis <trivernis@protonmail.com>
2024-07-13 11:21:45 +0200

    Fix shuffled 31x81 buttons

--- src/components/molecules/Buttons.svelte
+++ src/components/molecules/Buttons.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-  import { shuffle } from "$lib";
   import Button8831 from "../atoms/Button8831.svelte";
   import allButtons from "$lib/buttons";
+  import * as d3 from "d3-array";
+  import { onMount } from "svelte";
 
-  const buttons = $derived.by(() => {
-    const newList = [...allButtons];
-    shuffle(newList);
-    return newList;
+  const buttons = $state([...allButtons]);
+  onMount(() => {
+    d3.shuffle(buttons);
   });
 </script>
 
 <div class="buttons">
-  {#each buttons as button}
-    <Button8831 src={button.src} alt={button.alt} href={button.href} />
-  {/each}
+  {#key buttons}
+    {#each buttons as button (button.src)}
+      <Button8831 src={button.src} alt={button.alt} href={button.href} />
+    {/each}
+  {/key}
 </div>
 
 <style lang="scss">
@@ -26,6 +28,21 @@
       flex-direction: row;
       flex-wrap: wrap;
       justify-content: center;
+
+      animation-name: fade-in;
+      animation-duration: 2s;
+    }
+  }
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    25% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
     }
   }
 </style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -14,22 +14,6 @@ export function randInt(min: number, max: number): number {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export function shuffle<T>(array: T[]) {
-	let currentIndex = array.length;
-
-	// While there remain elements to shuffle...
-	while (currentIndex !== 0) {
-		// Pick a remaining element...
-		const randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex--;
-
-		// And swap it with the current element.
-		[array[currentIndex], array[randomIndex]] = [
-			array[randomIndex],
-			array[currentIndex],
-		];
-	}
-}
 export type ImageMetadata = {
 	caption?: string;
 	altText?: string;
