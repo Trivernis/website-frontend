2ee57eb9d7eecca85efc4bd3bed040ab37ef83e9
trivernis <trivernis@protonmail.com>
2025-10-02 21:51:45 +0200

    Add map

---
+++ src/routes/(plain)/wwo-25/02-maps/+page.svelte
@@ -0,0 +1,47 @@
+<div class="page">
+  <div class="back">
+    <a href="./">Back</a>
+  </div>
+  <div class="here">
+    <div class="pointer"></div>
+    <span>You are here</span>
+  </div>
+</div>
+
+<style lang="scss">
+  .page {
+    background-color: white;
+    color: black;
+    position: absolute;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    margin: 0;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  .back {
+    position: fixed;
+    margin: 1em;
+    a {
+      color: rgba(255, 0, 0, 0.5);
+    }
+  }
+  .here {
+    margin: auto;
+    .pointer {
+      background-color: red;
+      height: 5em;
+      width: 5em;
+      border-radius: 100%;
+      margin: auto;
+    }
+    span {
+      font-size: 20pt;
+      font-family: Lexend, Noto-Sans, Verdana, sans-serif;
+    }
+  }
+</style>
