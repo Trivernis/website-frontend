538af7e0721cf3e99956c40103f304f0e9af17ee
trivernis <trivernis@protonmail.com>
2025-10-04 23:31:35 +0200

    Add filter

--- src/routes/(plain)/wwo-25/04-filters/+page.svelte
+++ src/routes/(plain)/wwo-25/04-filters/+page.svelte
@@ -13,11 +13,16 @@
   <div class="back" role="application">
     <a href="./">Back</a>
   </div>
-  <img
-    src="/images/tokyo-rainbow-bridge.jpg"
-    alt="The rainbow bridge in Tokyo on a cloudy day. Phootographed from Odaiba."
-    class="test-image"
-  />
+  <div class="page-container">
+    <img
+      src="/images/tokyo-rainbow-bridge.jpg"
+      alt="The rainbow bridge in Tokyo on a cloudy day. Phootographed from Odaiba."
+      class="test-image"
+    />
+  </div>
+  <div class="page-container">
+    <div class="filter filter-red"></div>
+  </div>
 </div>
 
 <style lang="scss">
@@ -36,9 +41,29 @@
     bottom: 0;
     z-index: -200;
   }
+  .page-container {
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    top: 0;
+    left: 0;
+  }
   .test-image {
-    width: 30vw;
+    width: 45vw;
     height: auto;
     margin: auto;
+  }
+  .filter {
+    height: 30vw;
+    width: 30vw;
+    border-radius: 100%;
+    border: 15px solid black;
+    outline: 5px dotted black;
+    margin: auto;
+  }
+  .filter-red {
+    backdrop-filter: grayscale(1) brightness(1.2);
+    background: rgba(255, 0, 0, 0.5);
   }
 </style>
