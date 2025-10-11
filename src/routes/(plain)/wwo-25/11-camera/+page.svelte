ed3b6e74ac6aa1e6b9952ee8fc7711bd73016570
trivernis <trivernis@protonmail.com>
2025-10-11 18:05:05 +0200

    Add game version of focus

--- src/routes/(plain)/wwo-25/11-camera/+page.svelte
+++ src/routes/(plain)/wwo-25/11-camera/+page.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
   import { MetaTags } from "svelte-meta-tags";
   import kite from "$lib/assets/kite.jpg?enhanced";
+  import fish from "$lib/assets/fish.jpg?enhanced";
+  import nuremberg from "$lib/assets/nuremberg.jpg?enhanced";
+  import adler from "$lib/assets/adler.jpg?enhanced";
 
-  const focusTarget = Math.round(Math.random() * 100);
+  const images = [
+    {
+      img: kite,
+      alt: "A picture of a kite in the sky with a rainbow fish print.",
+    },
+    {
+      img: fish,
+      alt: "A picture of a white koi from the side swimming underwater with red and black spots",
+    },
+    {
+      img: nuremberg,
+      alt: `A picture of a bridge over a river in Nürnberg.
+      On top of the bridge the seems to be small connected houses that are connected.`,
+    },
+    {
+      img: adler,
+      alt: `A picture of the locomotive that served on the first railroad track in Germany between Nürnberg anf Fürth.
+      It is a small steam locomotive with a green boiler and red metal parts.`,
+    },
+  ];
 
+  let imageIndex = $state(0);
+  let focusTarget = $state(Math.round(Math.random() * 100));
   let sliderPosition = $state(0);
   let mouseDown = $state(false);
   let focusAmount = $state(0);
+  let victory = $state(false);
 
   let slider: HTMLDivElement | undefined;
 
@@ -29,6 +54,15 @@
         activeTouch = touch;
         moveSlider(mvX);
       }
+    }
+  }
+
+  function nextImage() {
+    if (imageIndex < images.length - 1) {
+      imageIndex = imageIndex + 1;
+      focusTarget = Math.round(Math.random() * 100);
+    } else {
+      victory = true;
     }
   }
 </script>
@@ -60,43 +94,67 @@
   </div>
   <h1 class="page-title">Focus the Image</h1>
   <div class="image-container">
-    <enhanced:img
-      src={kite}
-      alt="A picture of a kite"
-      class="sub"
-      fetchpriority="high"
-      style="filter: blur({Math.abs(focusTarget / 10 - focusAmount * 10)}px);"
-    />
+    {#if !victory}
+      <enhanced:img
+        src={images[imageIndex].img}
+        alt={images[imageIndex].alt}
+        class="sub"
+        fetchpriority="high"
+        style="filter: blur({Math.abs(focusTarget / 10 - focusAmount * 10)}px);"
+      />
+    {:else}
+      <h1 class="victory-banner">You are a professional photographer now!</h1>
+    {/if}
   </div>
-  <span
-    class="focus-amount"
-    style={Math.round(focusAmount * 100) === Math.round(focusTarget)
-      ? "color: #AF8"
-      : "color: #F78"}>{Math.round(focusAmount * 100)}</span
-  >
-  <div class="focus-slider" bind:this={slider}>
-    <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-    <div
-      role="application"
-      class="focus-slider-handle"
-      style="left: -{sliderPosition}px"
-      onmousedown={() => {
-        mouseDown = true;
-      }}
-      ontouchstart={(touch) => {
-        mouseDown = true;
-        activeTouch = touch.targetTouches.item(0) ?? undefined;
-      }}
+  {#if !victory}
+    <span
+      class="focus-amount"
+      style={Math.round(focusAmount * 100) === Math.round(focusTarget)
+        ? "color: #AF8"
+        : "color: #F78"}>{Math.round(focusAmount * 100)}</span
     >
-      {#each { length: 1000 } as _, i}
-        {#if i % 10 == 0}
-          <div class="focus-line focus-line-tall"></div>
-        {:else}
-          <div class="focus-line"></div>
-        {/if}
-      {/each}
+    <div class="focus-slider" bind:this={slider}>
+      <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+      <div
+        role="application"
+        class="focus-slider-handle"
+        style="left: -{sliderPosition}px"
+        onmousedown={() => {
+          mouseDown = true;
+        }}
+        ontouchstart={(touch) => {
+          mouseDown = true;
+          activeTouch = touch.targetTouches.item(0) ?? undefined;
+        }}
+      >
+        {#each { length: 1000 } as _, i}
+          {#if i % 10 == 0}
+            <div class="focus-line focus-line-tall"></div>
+          {:else}
+            <div class="focus-line"></div>
+          {/if}
+        {/each}
+      </div>
     </div>
-  </div>
+
+    <div class="button-container">
+      <button
+        class="button-next"
+        disabled={Math.round(focusAmount * 100) !== Math.round(focusTarget)}
+        style={Math.round(focusAmount * 100) !== Math.round(focusTarget)
+          ? "opacity: 0"
+          : "opacity: 1"}
+        onclick={() => nextImage()}
+      >
+        {#if imageIndex < images.length - 1}
+          Next
+        {:else}
+          Finish
+        {/if}
+      </button>
+    </div>
+  {/if}
+  <div class="bottom-pad"></div>
 </div>
 
 <style lang="scss">
@@ -131,6 +189,7 @@
     height: 70%;
     width: 70%;
     margin: auto;
+    display: flex;
 
     img {
       height: 100%;
@@ -154,6 +213,27 @@
   .focus-amount {
     font-size: 4em;
     margin: auto;
+  }
+
+  .button-container {
+    display: flex;
+  }
+
+  .button-next {
+    font-size: 1.5em;
+    padding: 0.5em 1em;
+    margin: 1em auto;
+    transition-duration: 0.5s;
+    color: white;
+    background-color: #a35;
+    border: none;
+    border-radius: 0.5em;
+    cursor: pointer;
+
+    &:active {
+      background-color: #b46;
+      transform: scale(90%);
+    }
   }
 
   .focus-slider {
@@ -185,6 +265,16 @@
         height: 80%;
       }
     }
+  }
+
+  .victory-banner {
+    font-size: 5em;
+    text-align: center;
+    margin: auto;
+  }
+
+  .bottom-pad {
+    height: 2em;
   }
 
   .back {
