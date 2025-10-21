4be0e5dfdf1cd463ba1062e92af0ca926823d706
trivernis <trivernis@protonmail.com>
2025-10-21 21:43:13 +0200

    Add jumpscare

--- src/routes/(plain)/wwo-25/21-language/+page.svelte
+++ src/routes/(plain)/wwo-25/21-language/+page.svelte
@@ -59,12 +59,18 @@
   @keyframes reveal {
     0% {
       opacity: 0;
+      transform: scale(0.5);
     }
-    99% {
+    90% {
       opacity: 0;
+      transform: scale(0.5);
+    }
+    95% {
+      transform: scale(2);
     }
     100% {
       opacity: 1;
+      transform: scale(1);
     }
   }
 </style>
